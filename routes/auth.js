c0971293729cd98d9b2210866d65bedfd16fb1b2
ghightower80 <ghightower80@yahoo.com>
2024-02-23 12:42:05 -0500

    fitst

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+
+/** Login: {username, password} => {token} */
+router.post("/login", async function (req, res, next) {
+  try {
+    const { username, password } = req.body;
+
+    if (await User.authenticate(username, password)) {
+      const token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Register user: registers, logs in, and returns token.
+ * {username, password, first_name, last_name, phone} => {token}.
+ */
+router.post("/register", async function (req, res, next) {
+  try {
+    const { username } = await User.register(req.body);
+    const token = jwt.sign({ username }, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
